c8cc0fd24384121fd5e04a188823055619ac7700
laxmimistry <118525460+laxmimistry@users.noreply.github.com>
2022-11-18 15:44:00 +0530

    first commit

---
+++ First_class.java
@@ -0,0 +1,15 @@
+package firstclass;
+
+public class First_class {
+
+	public static void main(String[] args) {
+		int a=5;
+		int b=6;
+		int sum=a+b;
+		
+		System.out.println("SUM:"+sum);
+				
+
+	}
+
+}
